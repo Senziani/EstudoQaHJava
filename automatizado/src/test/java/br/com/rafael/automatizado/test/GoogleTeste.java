3d78f0a19268c9c3a3f91f1c0414fa7c5fc91dfa
Rafael Senzini <rafael_senziani@hotmail.com>
2024-03-20 20:38:15 -0300

    Abrindo navegaor, pesquisando, validando informacao

--- automatizado/src/test/java/br/com/rafael/automatizado/test/GoogleTeste.java
+++ automatizado/src/test/java/br/com/rafael/automatizado/test/GoogleTeste.java
@@ -1,7 +1,13 @@
 package br.com.rafael.automatizado.test;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTeste {
@@ -13,8 +19,8 @@ public class GoogleTeste {
     private WebDriver driver;
 
     private void init() {
-        System.setProperty("webdriver.chrome.driver",chDriver);
-        driver =  new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", chDriver);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(urlCaminho);
     }
@@ -22,6 +28,15 @@ public class GoogleTeste {
     @Test
     public void deveAbrirGoogle() {
         init();
+
+        WebElement inputpesquisa = driver.findElement(By.name("q"));
+        inputpesquisa.sendKeys("Batata-Frita" + Keys.ENTER);
+
+        String verificaElemento = driver.findElement(By.id("result-stats")).getText();
+        assertTrue(verificaElemento.contains("Aproximadamente"),verificaElemento);
+
+
+        driver.close();
     }
 
 }
